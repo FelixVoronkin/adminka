5fe7ef816739f01feaaa3b99030fb09e85e28460
FelixVoronkin <118354953+FelixVoronkin@users.noreply.github.com>
2024-05-31 15:03:00 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,14 +1,13 @@
-const allowedCors = ["*","https://kibersiten.nomorepartiesco.ru","https://kibersitenfede.nomorepartiesco.ru"];
+const allowedCors = ["*","https://mybend.nomorepartiesco.ru/","https://myfend.nomorepartiesco.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
+  res.header("Access-Control-Allow-Origin", origin);
 
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,HEAD");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Con>
   next();
 }
 
 module.exports = cors;
+
